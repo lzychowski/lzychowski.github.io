6084fe8c402c9ea29625771a93421727cfebfb9a
unknown <Leszek Zychowski>
2015-07-19 22:04:52 -0500

    added google analytics

---
+++ assets/scripts/highlight_helper.js
@@ -0,0 +1,25 @@
+/**
+ * Created by leszek on 7/19/2015.
+ */
+$(window).resize(function(){
+	$(".highlight span").each(function(){
+		var current = $(this);
+		var text = current.text();
+
+		var words = text.split(' ');
+
+		current.text(words[0]);
+		var height = current.height();
+
+		for(var i = 1; i < words.length; i++){
+			current.text(current.text() + ' ' + words[i]);
+
+			if(current.height() > height){
+				height = current.height();
+				console.log(words[i-1]);
+			}
+		}
+
+		console.log(current);
+	});
+});
